1d5a6b2403b67c04499087b0ea617fd9c09a58f2
NT-ThuHang <63140446+NT-ThuHang@users.noreply.github.com>
2020-04-25 20:07:34 +0700

    Update 25.04.20.07

--- SourceCode/QInt.h
+++ SourceCode/QInt.h
@@ -6,7 +6,12 @@ using namespace std;
 class QInt
 {
 private:
-	int data[4];
+	unsigned int data[4];
+	//explain
+	//Domain of data[0] is [0,2^32] 
+	//Domain of data[1] is [0,2^32] 
+	//Domain of data[2] is [0,2^32] 
+	//Domain of data[3] is [-2^31,2^32] 
 public:
 	void DivideString(string& a, int k);
 	friend istream& operator >> (istream&, QInt&);
@@ -14,6 +19,8 @@ public:
 	QInt();
 	~QInt();
 };
+
+
 
 
 
